a5dd5b27d8ac81f16e6b3319c710913b0aaf6d3d
김예린 <annalin1004@gmail.com>
2024-11-28 02:08:13 +0900

    :recycle: 공부하는 파트 기본 코드 추가
    <2024-11-28> 김예린
    비선형 자료구조의 기본 제공 코드와 알고리즘의 기본 제공 코드를 파트별로 나눠서 추가함

---
+++ src/main/java/me/kimyelin/part05/ProblemSolving_02/src/Practice4.java
@@ -0,0 +1,14 @@
+package me.kimyelin.part05.ProblemSolving_02.src;
+
+public class Practice4 {
+    public static int solution(int cacheSize, String[] searches) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        String[] searches = {"Google", "Google", "Microsoft", "Apple", "Qualcomm", "Google", "Qualcomm"};
+        System.out.println(solution(3, searches));
+    }
+}
